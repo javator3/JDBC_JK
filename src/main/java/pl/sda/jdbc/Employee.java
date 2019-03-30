bf6768965aa830ef83a189370f5944b66ac48205
Pandragona <asiuspospolitus@gmail.com>
2019-03-30 13:11:23 +0100

    adding logger dependencies, object creation based on queries SELECT from my database

---
+++ src/main/java/pl/sda/jdbc/Employee.java
@@ -0,0 +1,23 @@
+package pl.sda.jdbc;
+
+public class Employee {
+
+    private int id;
+    private String name;
+    private int salary;
+
+    public Employee(int id, String name, int salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
--- src/main/java/pl/sda/jdbc/Statements.java
+++ src/main/java/pl/sda/jdbc/Statements.java
@@ -1,19 +1,31 @@
 package pl.sda.jdbc;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Statements {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ksiegarnia?serverTimezone=UTC","root", "Sm0cz0J3st");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ksiegarnia?serverTimezone=UTC&logger=com.mysql.cj.log.Slf4JLogger&profileSQL=true","root", "Sm0cz0J3st");
 
 //     createTable(connection);
 //     insert5newEmployee(connection);
-//        insert5newEmployee2(connection);
-        deleteEmplyeeWhen(connection);
+        insert5newEmployee2(connection);
+//        deleteEmployeeWhen2(connection,1000);
+//        deleteEmplyeeWhen(connection, 1000);
+//        selectNameAndSalaryFromTable(connection);
+//        selectNmeAndSalaryFromTableSortedByName(connection);
+//        selectNmeAndSalaryFromTableWereSalaryIsHigherThan1000(connection);
+//        selectNameAndSalaryFromTableWereNameEndsWith4(connection);
 //     dropTable(connection);
+
+
+        //Zadanie 17
+//        List<Employee> employees = selectEmployee(connection);
+//        System.out.println(employees.toString());
 
     }
 
@@ -48,9 +60,19 @@ public class Statements {
 
     //metoda która usuwa wiersze z pracownikami o zarobkach mniejszych niż podana przez użytkownika
 
-    static void deleteEmplyeeWhen(Connection connection) throws SQLException {
+    static void deleteEmployeeWhen2(Connection connection, int salaryBorder) throws SQLException {
+        Statement statement = connection.createStatement();
+        String formattedString = String.format("DELETE FROM employee WHERE salary < %d", salaryBorder);
+
+        statement.executeUpdate(formattedString);
+    }
+
+
+    //metoda która usuwa wiersze z pracownikami o zarobkach mniejszych niż podana przez użytkownika (prepared statement)
+
+    static void deleteEmplyeeWhen(Connection connection, int salary) throws SQLException {
         PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM employee WHERE salary < ?");
-        preparedStatement.setInt(1, 1000);
+        preparedStatement.setInt(1, salary);
         preparedStatement.executeUpdate();
     }
 
@@ -59,6 +81,73 @@ public class Statements {
     static void dropTable(Connection connection) throws SQLException {
         Statement statement = connection.createStatement();
         statement.execute("DROP TABLE employee");
+    }
+
+    //Wypisz wszystkich pracowikow
+
+    static void selectNameAndSalaryFromTable(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT name, salary FROM employee");
+
+        while (resultSet.next()){
+            String name = resultSet.getString(1); //można wwpisać nr kolumny
+           int salary  = resultSet.getInt("salary"); //lub nazwe kolumny tabeli
+            System.out.println(name + " " + salary);
+        }
+
+    }
+
+    //Wypisz wszystkich pracownikow posortowanych wg imienia
+
+    static void selectNmeAndSalaryFromTableSortedByName(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT name, salary FROM employee ORDER BY name ASC");
+        while (resultSet.next()){
+            String name = resultSet.getString(1); //można wwpisać nr kolumny
+            int salary  = resultSet.getInt("salary"); //lub nazwe kolumny tabeli
+            System.out.println(name + " " + salary);
+        }
+
+    }
+
+    static void selectNmeAndSalaryFromTableWereSalaryIsHigherThan1000(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT name, salary FROM employee WHERE salary > 1000");
+        while (resultSet.next()){
+            String name = resultSet.getString(1); //można wwpisać nr kolumny
+            int salary  = resultSet.getInt("salary"); //lub nazwe kolumny tabeli
+            System.out.println(name + " " + salary);
+        }
+
+    }
+
+    static void selectNameAndSalaryFromTableWereNameEndsWith4(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT name, salary FROM employee WHERE name LIKE '%4'");
+        while (resultSet.next()){
+            String name = resultSet.getString(1); //można wwpisać nr kolumny
+            int salary  = resultSet.getInt("salary"); //lub nazwe kolumny tabeli
+            System.out.println(name + " " + salary);
+        }
+
+    }
+
+    //metoda który stworzy troche obiektowosci
+    //
+
+    static List<Employee> selectEmployee(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT id, name, salary from employee");
+        List<Employee> result = new ArrayList<Employee>();
+        while (resultSet.next()){
+            int id = resultSet.getInt("id");
+            String name = resultSet.getString(2);
+            int salary = resultSet.getInt(3);
+            Employee employee = new Employee(id, name, salary);
+            result.add(employee);
+        }
+
+        return result;
     }
 
 }
