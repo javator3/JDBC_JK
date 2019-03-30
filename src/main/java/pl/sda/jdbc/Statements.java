e64ff3379d5edc6e65a8a944cb843528a58ba440
Pandragona <asiuspospolitus@gmail.com>
2019-03-30 11:48:45 +0100

    prepaeStatement for deleting employees when salary < ?

--- src/main/java/pl/sda/jdbc/Statements.java
+++ src/main/java/pl/sda/jdbc/Statements.java
@@ -1,9 +1,7 @@
 package pl.sda.jdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Random;
 
 public class Statements {
 
@@ -12,7 +10,10 @@ public class Statements {
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ksiegarnia?serverTimezone=UTC","root", "Sm0cz0J3st");
 
 //     createTable(connection);
-     insert5newEmployee(connection);
+//     insert5newEmployee(connection);
+//        insert5newEmployee2(connection);
+        deleteEmplyeeWhen(connection);
+//     dropTable(connection);
 
     }
 
@@ -24,7 +25,7 @@ public class Statements {
 
     }
 
-    //metoda która
+    //metoda która zapisuje 5 pracowików
 
     static void insert5newEmployee(Connection connection) throws SQLException {
         Statement statement = connection.createStatement();
@@ -32,6 +33,32 @@ public class Statements {
             String formattedString = String.format("INSERT INTO employee(name,salary) values('name%s',%d)", "Jan"+i, 10*i);
             statement.executeUpdate(formattedString);
         }
+    }
+
+    //metoda zapisywania pracowników za pomocą prepare statement
+
+    static void insert5newEmployee2(Connection connection) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO employee(name, salary) values (?,?)");
+        for (int i = 0; i<5 ; i++){
+            preparedStatement.setString(1, "imie"+i);
+            preparedStatement.setInt(2,new Random().nextInt(2000));
+            preparedStatement.executeUpdate(); //zawsze pamietac o execute update!!!!
+        }
+    }
+
+    //metoda która usuwa wiersze z pracownikami o zarobkach mniejszych niż podana przez użytkownika
+
+    static void deleteEmplyeeWhen(Connection connection) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM employee WHERE salary < ?");
+        preparedStatement.setInt(1, 1000);
+        preparedStatement.executeUpdate();
+    }
+
+    //metoda która usuwa tabelę
+
+    static void dropTable(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.execute("DROP TABLE employee");
     }
 
 }
